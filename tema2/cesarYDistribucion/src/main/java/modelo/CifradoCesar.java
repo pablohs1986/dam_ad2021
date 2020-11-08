d40d737920fef2ba5759b31602896ec6910496c1
pHerrero <pabloherrero1986@gmail.com>
2020-11-08 19:18:51 +0100

    Working on ej4.: Working on cifrarArchivoConCesar v2

--- tema2/cesarYDistribucion/src/main/java/modelo/CifradoCesar.java
+++ tema2/cesarYDistribucion/src/main/java/modelo/CifradoCesar.java
@@ -5,12 +5,17 @@
  */
 package modelo;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,7 +31,7 @@ public class CifradoCesar {
         try {
             File[] listadoArchivosTexto = listarArchivosTextoEnDirectorio(directorio);
             for (File archivoTexto : listadoArchivosTexto) {
-                cifrarArchivoConCesar(archivoTexto, contadorFicherosCifrados, 5);
+                cifrarArchivoConCesar(archivoTexto, contadorFicherosCifrados, clave);
                 contadorFicherosCifrados++;
             }
         } catch (Exception ex) {
@@ -60,31 +65,163 @@ public class CifradoCesar {
         File archivoCifrado = new File("textos" + File.separator + "archivoCifrado" + numeroArchivo + ".txt");
 
         try {
-            FileInputStream fis = new FileInputStream(archivoTexto);
-            FileOutputStream fos = new FileOutputStream(archivoCifrado, true);
-
-            System.out.println("\n" + "Cifrando el archivo " + "\"" + archivoTexto.getName() + "\"" + "...");
-
-            int caracterLeido;
-            int caracterCifrado;
-
-            while ((caracterLeido = fis.read()) != -1) {
-                caracterCifrado = caracterLeido + desplazamiento;
-                fos.write(caracterCifrado);
-                fos.flush();
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(archivoTexto));
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(archivoCifrado));
+            
+            int caracterLeido = 0;
+            int caracterCifrado = 0;
+            
+            while (caracterLeido != -1) {
+                caracterLeido = reader.read();
+                
+                // Abecedario mayúsculas
+                if (caracterLeido >= 65 && caracterLeido <= 90) {
+                    caracterCifrado = caracterLeido + desplazamiento;
+                    if (caracterCifrado >= 90) {
+                        caracterCifrado = caracterCifrado - 25;
+                        writer.write(caracterCifrado);
+                        writer.flush();
+                    } else {
+                        writer.write(caracterCifrado);
+                        writer.flush();
+                    }
+                // Abecedario minúsculas
+                } else if (caracterLeido >= 97 && caracterLeido <= 122) {
+                    caracterCifrado = caracterLeido + desplazamiento;
+                    if (caracterCifrado >= 122) {
+                        caracterCifrado = caracterCifrado - 25;
+                        writer.write(caracterCifrado);
+                        writer.flush();
+                    } else {
+                        writer.write(caracterCifrado);
+                        writer.flush();
+                    }
+                // Ñ
+                } else if (caracterLeido == 209) {
+                    caracterCifrado = 78 + desplazamiento;
+                    writer.write(caracterCifrado);
+                } else if (caracterLeido == 241) {
+                    caracterCifrado = 110 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                // Acentos minúsculas
+                } else if (caracterLeido == 225) {
+                    caracterCifrado = 97 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 233) {
+                    caracterCifrado = 101 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 237) {
+                    caracterCifrado = 105 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 243) {
+                    caracterCifrado = 111 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 250) {
+                    caracterCifrado = 117 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                // Acentos mayúsculas
+                } else if (caracterLeido == 193) {
+                    caracterCifrado = 65 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 201) {
+                    caracterCifrado = 69 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 205) {
+                    caracterCifrado = 73 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 211) {
+                    caracterCifrado = 79 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                } else if (caracterLeido == 218) {
+                    caracterCifrado = 85 + desplazamiento;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                // Resto caracteres
+                } else {
+                    caracterCifrado = caracterLeido;
+                    writer.write(caracterCifrado);
+                    writer.flush();
+                }
+                
             }
-
-            System.out.println("\"" + archivoTexto.getName() + "\"" + " cifrado. Guardado como " + "\"" + archivoCifrado.getName() + "\"");
-
-            fis.close();
-            fos.close();
-
+//        try {
+//            FileInputStream fis = new FileInputStream(archivoTexto);
+////            FileOutputStream fos = new FileOutputStream(archivoCifrado, true);
+//
+//            System.out.println("\n" + "Cifrando el archivo " + "\"" + archivoTexto.getName() + "\"" + "...");
+//
+//            int codigoAsciiLeido = 0;
+//            int codigoAsciiCifrado = 0;
+//            char caracterLeido;
+//
+//            while (codigoAsciiLeido != -1) {
+//                codigoAsciiLeido = fis.read();
+//                System.out.println("ascii leido " + codigoAsciiLeido);
+//                
+//                caracterLeido = (char) codigoAsciiLeido;
+//                System.out.println("caracter leido " + caracterLeido);
+//
+//
+////                fos.write(caracterCifrado);
+////                fos.flush();
+//            }
+//
+//            System.out.println("\"" + archivoTexto.getName() + "\"" + " cifrado. Guardado como " + "\"" + archivoCifrado.getName() + "\"");
+//
+//            fis.close();
+////            fos.close();
+//
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(CifradoCesar.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(CifradoCesar.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(CifradoCesar.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(CifradoCesar.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+//    public void cifrarArchivoConCesar(File archivoTexto, int numeroArchivo, int desplazamiento) {
+//        File archivoCifrado = new File("textos" + File.separator + "archivoCifrado" + numeroArchivo + ".txt");
+//
+//        try {
+//            FileInputStream fis = new FileInputStream(archivoTexto);
+//            FileOutputStream fos = new FileOutputStream(archivoCifrado, true);
+//
+//            System.out.println("\n" + "Cifrando el archivo " + "\"" + archivoTexto.getName() + "\"" + "...");
+//
+//            int caracterLeido;
+//            int caracterCifrado;
+//
+//            while ((caracterLeido = fis.read()) != -1) {
+//                caracterCifrado = caracterLeido + desplazamiento;
+//                fos.write(caracterCifrado);
+//                fos.flush();
+//            }
+//
+//            System.out.println("\"" + archivoTexto.getName() + "\"" + " cifrado. Guardado como " + "\"" + archivoCifrado.getName() + "\"");
+//
+//            fis.close();
+//            fos.close();
+//
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(CifradoCesar.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(CifradoCesar.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 
     public void descifrarArchivoConCesar(File archivoTexto, int numeroArchivo, int desplazamiento) {
         File archivoDescifrado = new File("textos" + File.separator + "archivoDescifrado" + numeroArchivo + ".txt");
